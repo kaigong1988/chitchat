2d61780ab820c35b4d1d7d0766622343ac1fcda2
Kai Gong <kaigong@Kais-MacBook-Pro.local>
2021-02-05 00:23:38 -0500

    webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,23 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules,
+        exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
   },
-  plugins: [
-    new HtmlWebpackPlugin(
-      {
-        template: './dist/index.html'
-      }
-    )
-  ]
-}
+};
